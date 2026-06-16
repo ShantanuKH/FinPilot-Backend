a04c8a94c8a89298244e6f07d8f93a520533b6be
ShantanuKH <khadseshantanu02@gmail.com>
2026-06-16 18:26:59 +0530

    Initial FinPilot setup

---
+++ src/test/java/com/shantanu/FinPilot/FinPilotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shantanu.FinPilot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinPilotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
