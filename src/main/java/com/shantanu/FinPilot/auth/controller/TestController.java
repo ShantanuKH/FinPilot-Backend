13431f3424e72d7beb116f215013414d2ca76648
ShantanuKH <khadseshantanu02@gmail.com>
2026-06-26 13:12:15 +0530

    Built a complete JWT authentication system.

---
+++ src/main/java/com/shantanu/FinPilot/auth/controller/TestController.java
@@ -0,0 +1,20 @@
+package com.shantanu.FinPilot.auth.controller;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/test")
+public class TestController {
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "Hello Authenticated User";
+    }
+    @GetMapping("/me")
+    public String me(Authentication authentication) {
+        return authentication.getName();
+    }
+}
